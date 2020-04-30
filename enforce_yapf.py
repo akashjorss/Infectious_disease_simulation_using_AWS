5b0d86cfa2020b0dcaca750e80cbb4f2c66ecb23
akashjorss <akash.jorss@gmail.com>
2020-04-30 20:31:34 +0200

    yapf and pip freeze automated

---
+++ enforce_yapf.py
@@ -0,0 +1,10 @@
+from yapf.yapflib.yapf_api import FormatFile  # reformat a file
+import os
+rootdir = './'
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith(".py"):
+            file_path = os.path.join(subdir, file)
+            #print(filepath)
+            FormatFile(file_path, in_place=True, style_config='pep8')
+
