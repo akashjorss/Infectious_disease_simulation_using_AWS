2eea822304eb6102ebd4a384867eac85da6b905d
emyhr <enurmatbekov@gmail.com>
2020-05-07 17:53:58 +0200

    terraform

---
+++ terraform/lambda.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+region = "eu-west-1"
+}
+
+resource "aws_lambda_function" "lambda_package" {
+function_name = "python_lambda"
+
+# The bucket name as created earlier with "aws s3api create-bucket"
+s3_bucket = "cc-project-2020"
+s3_key    = "lambda_package.zip"
+
+# "main" is the filename within the zip file (main.js) and "handler"
+# is the name of the property under which the handler function was
+# exported in that file.
+handler = "lambda_function.handler"
+runtime = "python3.8"
+
+role = aws_iam_role.lambda_exec.arn
+}
+
+# IAM role which dictates what other AWS services the Lambda function
+# may access.
+resource "aws_iam_role" "lambda_exec" {
+name = "lambda_role"
+assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Action": "sts:AssumeRole",
+"Principal": {
+"Service": "lambda.amazonaws.com"
+},
+"Effect": "Allow",
+"Sid": ""
+}
+]
+}
+EOF
+}
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_package.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*"
+}
