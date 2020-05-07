a13724a43fb6a21d1ef3da30ad6ed9ad4631189a
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2020-05-07 22:45:37 +0200

    Remove redundant tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,4 @@
 import json
-from io import BytesIO
-
-import pycurl
 
 from aws_lambda.hello_world import hello_world_lambda_handler
 from .context import blueprint
@@ -14,29 +11,6 @@ def test_app(capsys, example_fixture):
     captured = capsys.readouterr()
 
     assert "Hello World..." in captured.out
-
-    # test the output of lambda
-    b_obj = BytesIO()
-    crl = pycurl.Curl()
-
-    # Set URL value
-    crl.setopt(crl.URL, "https://dvgdt3t23b.execute-api.us-east-2.amazonaws.com/test")
-
-    # Write bytes that are utf-8 encoded
-    crl.setopt(crl.WRITEDATA, b_obj)
-
-    # Perform a file transfer
-    crl.perform()
-
-    # End curl session
-    crl.close()
-
-    # Get the content stored in the BytesIO object (in byte characters)
-    response = json.loads(b_obj.getvalue().decode("utf8"))["response"]
-
-    # Decode the bytes stored in get_body to HTML and print the result
-    # print('Output of GET request:\n%s' % response)
-    assert response == "Hello World"
 
 
 def test_lambda():
