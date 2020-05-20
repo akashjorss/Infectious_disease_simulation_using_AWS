2693b4ee9d1a08847c6a0e452c7fd1e798a0b5a2
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2020-05-20 23:08:42 +0200

    Proper cache path for venv

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -4,10 +4,9 @@ set -x
 
 python --version
 # Test inside virtualenv to not break AWS CLI
-python -m venv /tmp/venv
+python -m venv /tmp/cc_project_python/venv
 echo "Created VirtualEnv"
-ls /tmp/venv
-source /tmp/venv/bin/activate
+source /tmp/cc_project_python/venv/bin/activate
 echo "Activated VirtualEnv"
 pip install -r requirements.txt
 pytest
