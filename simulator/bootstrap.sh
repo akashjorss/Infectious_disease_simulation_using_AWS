dc048747a170e4d8eb671a9140ec67736d1e70f3
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2020-05-21 14:03:40 +0200

    Point to new artefact locatino

--- simulator/bootstrap.sh
+++ simulator/bootstrap.sh
@@ -4,5 +4,5 @@ sudo pip3 install --upgrade pip
 python3 -m pip install --upgrade setuptools wheel pip
 mkdir -p ~/app
 cd ~/app || exit
-aws s3 cp s3://cc-project-simulation-app . --recursive
+aws s3 cp s3://311807373494-cc-project-simulation-app . --recursive
 python3 -m pip --no-cache-dir install -r requirements.txt
