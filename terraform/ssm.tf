14acfc056dee13f33f236c3d61187559d3eefc35
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2020-05-26 18:47:25 +0200

    Secure parameters

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -21,3 +21,35 @@ resource "aws_ssm_parameter" "simulation_lambda_params" {
     version]
   }
 }
+
+
+locals {
+  spark_es_params = [
+    "cloud_id",
+    "username",
+  "password"]
+}
+
+locals {
+  spark_simulation_app = "spark_simulation_app"
+}
+
+resource "aws_kms_key" "es_secret_key" {
+  description             = "Key for encrypting ES credentials for use in "
+  deletion_window_in_days = 7
+}
+
+
+
+resource "aws_ssm_parameter" "simulation_spark_secure_params" {
+  count  = length(local.spark_es_params)
+  name   = "/${local.spark_simulation_app}/${local.spark_es_params[count.index]}"
+  type   = "SecureString"
+  value  = "default"
+  key_id = aws_kms_key.es_secret_key.key_id
+  lifecycle {
+    ignore_changes = [
+      value,
+    version]
+  }
+}
